103fe4fa55bcee98b45d51fdd79a5342e5ea7e3b
Benny <blaze349@protonmail.com>
2017-04-15 15:03:40 +0000

    Added documentation and improved static functions

--- util/vector.js
+++ util/vector.js
@@ -1,14 +1,15 @@
 class Vector {
+    //constructor
     constructor(x, y) {
         this.x = x
         this.y = y
     }
     
+    //non-static functions
     add(vec2) {
         this.x += vec2.x
         this.y += vec2.y
     }
-    
     sub(vec2) {
         this.x -= vec2.x
         this.y -= vec2.y
@@ -22,22 +23,18 @@ class Vector {
         this.x /= val
         this.y /= val
     }
-    
+    //static functions. Called with function prototype
     static add(vec1, vec2) {
-        vec1.x += vec2.x
-        vec1.y += vec2.y
+        return new Vector(vec1.x += vec2.x,vec1.y += vec2.y)
     }
     static sub(vec1, vec2) {
-        vec1.x -= vec2.x
-        vec1.y -= vec2.y
+        return new Vector(vec1.x -= vec2.x,vec1.y -= vec2.y)
     }
     static mult(vec1, val) {
-        vec1.x *= val
-        vec2.y *= val
+        return new Vector(vec1.x * val, vec1.y * val)
     }
     static div(vec1, val) {
-        vec1.x /= val
-        vec2.y /= val
+        return new Vector(vec1.x / val, vec1.y / val)
     }
     
 }
