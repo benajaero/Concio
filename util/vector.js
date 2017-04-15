5e750be8ff8385ca21229ef0b4b7e696c2164455
Benny <blaze349@protonmail.com>
2017-04-15 15:22:30 +0000

    Set magnitude

--- util/vector.js
+++ util/vector.js
@@ -27,6 +27,23 @@ class Vector {
     length() {
         return Math.sqrt((this.x * this.x) + (this.y * this.y))
     }
+    
+    angle() {
+        /* We have the opposite and adjacent side
+        We want to find the angle between the hypotenuse and the adjacent side
+        We can use the inverse tan function
+        */
+        return Math.atan2(this.y, this.x)
+    }
+    setLength(val) {
+        /* 1. Find the angle
+        2. Change the hypotenuse
+        3. Find the new side lengths by using the angle
+        */
+        let angle = this.angle()
+        this.x = val * Math.cos(angle)
+        this.y = val * Math.sin(angle)
+    }
     //static functions. Called with function prototype
     static add(vec1, vec2) {
         return new Vector(vec1.x += vec2.x,vec1.y += vec2.y)
